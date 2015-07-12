add3c398aeaf9fdaabfb0874bd6fe4c3c98d9ab2
Romel Campbell <romel.campbell@regeneron.com>
2015-07-12 05:03:31 -0400

    master #comment Using subl3

---
+++ ts/Car.js
@@ -0,0 +1,18 @@
+var Engine = (function () {
+    function Engine(hoursePower, engineType) {
+        this.hoursePower = hoursePower;
+        this.engineType = engineType;
+    }
+    return Engine;
+})();
+var Car = (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    return Car;
+})();
+// window.onload = function() {
+// 	var car = new Car('V8');
+// 	car.start();
+// 	car.stop();
+// } 
--- ts/Car.ts
+++ ts/Car.ts
@@ -1,24 +1,19 @@
+class Engine {
+	constructor(public hoursePower: number, 
+		        public engineType: string){
+
+	}
+}
+
 class Car {
-
-	engine: string;
-
-
-	constructor(engine:string){
-		this.engine = engine;
-	}
-
-	start() {
-		alert('Engine started: ' + this.engine);
-	}
-
-
-	stop() {
-		alert('Engine stopped: ' + this.engine);
+	engine: Engine
+    constructor(public engine: Engine){
+        this.engine = engine;
 	}
 }
 
-window.onload = function() {
-	var car = new Car('V8');
-	car.start();
-	car.stop();
-}
+// window.onload = function() {
+// 	var car = new Car('V8');
+// 	car.start();
+// 	car.stop();
+// }
