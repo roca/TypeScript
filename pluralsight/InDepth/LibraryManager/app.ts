f785adecc6a957859164dd40170d0361832dc33d
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-05-16 04:10:40 -0400

    Enums and Arrays

--- pluralsight/InDepth/LibraryManager/app.ts
+++ pluralsight/InDepth/LibraryManager/app.ts
@@ -1,9 +1,9 @@
 function GetAllBooks() {
   let books = [
-    { title: "Ulsses", author: "James Joyce", available: true },
-    { title: "Farewell to Arms", author: "Ernest Hemingway", available: false },
-    { title: "I Know Why the Caged Bird Sings", author: "Maya Angelou", available: true },
-    {title: "Moby Dick", author: "Herman Melville", available: true},
+    { title: "Ulsses", author: "James Joyce", available: true, category: Category.Fiction },
+    { title: "Farewell to Arms", author: "Ernest Hemingway", available: false, category: Category.Fiction },
+    { title: "I Know Why the Caged Bird Sings", author: "Maya Angelou", available: true, category: Category.Poetry },
+    {title: "Moby Dick", author: "Herman Melville", available: true, category: Category.Fiction},
   ];
 
   return books;
@@ -27,5 +27,18 @@ function LogFirstAvailable(books): void {
     console.log("First Available: " + firstAvailable);
 }
 
+enum Category { Biography, Poetry, Fiction, History, Children }
+
+function GetBookTitlesByCategory(categoryFilter: Category): Array<string> {
+
+  console.log("Getting books in category: " + categoryFilter);
+
+  const allBooks = GetAllBooks();
+
+  return allBooks.
+  filter( book => book.category === categoryFilter ).
+  map( book => book.title );
+
+}
 const allBooks = GetAllBooks();
 LogFirstAvailable(allBooks);
--- pluralsight/InDepth/LibraryManager/js/app.js
+++ pluralsight/InDepth/LibraryManager/js/app.js
@@ -1,9 +1,9 @@
 function GetAllBooks() {
     var books = [
-        { title: "Ulsses", author: "James Joyce", available: true },
-        { title: "Farewell to Arms", author: "Ernest Hemingway", available: false },
-        { title: "I Know Why the Caged Bird Sings", author: "Maya Angelou", available: true },
-        { title: "Moby Dick", author: "Herman Melville", available: true },
+        { title: "Ulsses", author: "James Joyce", available: true, category: Category.Fiction },
+        { title: "Farewell to Arms", author: "Ernest Hemingway", available: false, category: Category.Fiction },
+        { title: "I Know Why the Caged Bird Sings", author: "Maya Angelou", available: true, category: Category.Poetry },
+        { title: "Moby Dick", author: "Herman Melville", available: true, category: Category.Fiction },
     ];
     return books;
 }
@@ -19,6 +19,21 @@ function LogFirstAvailable(books) {
     }
     console.log("Total Books: " + numberOfBooks);
     console.log("First Available: " + firstAvailable);
+}
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+function GetBookTitlesByCategory(categoryFilter) {
+    console.log("Getting books in category: " + categoryFilter);
+    var allBooks = GetAllBooks();
+    return allBooks.
+        filter(function (book) { return book.category === categoryFilter; }).
+        map(function (book) { return book.title; });
 }
 var allBooks = GetAllBooks();
 LogFirstAvailable(allBooks);
