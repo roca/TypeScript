13753dffd25f1390ad5531595306a5e0c996a512
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-09-27 04:08:01 -0400

    Loops: WIP

--- pluralsight/practical_start/vendingmachine/js/app.js
+++ pluralsight/practical_start/vendingmachine/js/app.js
@@ -17,20 +17,77 @@ var Quarter = (function () {
     };
     return Quarter;
 }());
+var SodaCategory = (function () {
+    function SodaCategory() {
+        this.name = "Soda";
+    }
+    SodaCategory.prototype.getImageUrl = function () {
+        return "img/SodaCan.png";
+    };
+    return SodaCategory;
+}());
+/// <reference path="./productCategory.ts" />
+var CocaCola = (function () {
+    function CocaCola() {
+        this.name = "Coca-Cola";
+        this.price = 2.30;
+        this.category = new SodaCategory();
+    }
+    return CocaCola;
+}());
+/// <reference path="./product.ts" />
+var productFactory = (function () {
+    function productFactory() {
+    }
+    productFactory.GetProduct = function () {
+        return new CocaCola();
+    };
+    return productFactory;
+}());
 /// <reference path="./coin.ts" />
+/// <reference path="./product.ts" />
+/// <reference path="./productFactory.ts" />
+var VendingMachineSize;
+(function (VendingMachineSize) {
+    VendingMachineSize[VendingMachineSize["small"] = 6] = "small";
+    VendingMachineSize[VendingMachineSize["medium"] = 9] = "medium";
+    VendingMachineSize[VendingMachineSize["large"] = 12] = "large";
+})(VendingMachineSize || (VendingMachineSize = {}));
+var Cell = (function () {
+    function Cell(product) {
+        this.product = product;
+        this.stock = ko.observable(3);
+        this.sold = ko.observable(false);
+    }
+    return Cell;
+}());
 var VendingMachine = (function () {
     function VendingMachine() {
         var _this = this;
         this.paid = ko.observable(0);
+        this.cells = ko.observableArray([]);
         this.acceptedCoins = [new Quarter()];
         this.acceptCoin = function (coin) {
             var oldTotal = _this.paid();
             _this.paid(oldTotal + coin.Value);
         };
     }
+    Object.defineProperty(VendingMachine.prototype, "size", {
+        set: function (givenSize) {
+            this.cells([]);
+            for (var index = 0; index < givenSize; index++) {
+                var product = productFactory.GetProduct();
+                this.cells.push(new Cell(product));
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
     return VendingMachine;
 }());
 /// <reference path="vendingMachine.ts" />
+/// <reference path="typings/knockout.d.ts" />
 var machine = new VendingMachine();
+machine.size = VendingMachineSize.medium;
 ko.applyBindings(machine);
 //# sourceMappingURL=app.js.map
--- pluralsight/practical_start/vendingmachine/ts/bootstrapper.ts
+++ pluralsight/practical_start/vendingmachine/ts/bootstrapper.ts
@@ -1,6 +1,9 @@
 /// <reference path="vendingMachine.ts" />
+/// <reference path="typings/knockout.d.ts" />
+
 
 
 
 var machine = new VendingMachine();
+machine.size = VendingMachineSize.medium;
 ko.applyBindings(machine);
